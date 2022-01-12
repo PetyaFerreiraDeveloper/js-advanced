164bb88393f503d81f7eee99f7a8bee52d42bbe9
Petya Ferreira <petianaidenova@gmail.com>
2022-01-12 08:44:03 +0100

    remove comments

--- js-advanced-january/week1/3-lab-syntax-functions-statements/06-sum.js
+++ js-advanced-january/week1/3-lab-syntax-functions-statements/06-sum.js
@@ -2,13 +2,12 @@ function sumNumbers(n, m) {
     let firstNumber = Number(n);
     let secondNumber = Number(m);
 
-    // let firstNumber = Math.min(Number(n), Number(m));
-    // let secondNumber = Math.max(Number(n), Number(m));
     let sum = 0;
     for (let i = firstNumber; i <= secondNumber; i++) {
         sum += i;
     }
+    
     return sum;
 }
 
-sum(3, 1)
+sumNumbers(3, 1)
