87a6ee40be1080ba84f112c56977a8beccae7694
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2022-01-27 23:15:48 +0100

    lab-events-exercise

--- js-advanced-january/week3/12-excercise-dom-manipulation/05.JS-Advanced-DOM-Events-Exercise-Resources/02.Time-Converter/solution.js
+++ js-advanced-january/week3/12-excercise-dom-manipulation/05.JS-Advanced-DOM-Events-Exercise-Resources/02.Time-Converter/solution.js
@@ -1,4 +1,36 @@
 function attachEventsListeners() {
-
-    console.log('TODO:...');
+    let convertBtnElements = document.querySelectorAll('input[type="button"]'); //NodeList
+    let daysInputElement = document.getElementById("days");
+    let hoursInputElement = document.getElementById("hours");
+    let minutesInputElement = document.getElementById("minutes");
+    let secondsInputElement = document.getElementById("seconds");
+    
+    for (let btn of convertBtnElements) {
+        btn.addEventListener("click", display);
+        function display() {
+            let btnId = btn.id;
+            switch (btnId) {
+                case "daysBtn":
+                    hoursInputElement.value = 24 * Number(daysInputElement.value);
+                    minutesInputElement.value = Number(hoursInputElement.value) * 60;
+                    secondsInputElement.value = Number(minutesInputElement.value) * 60;
+                    break;
+                case "hoursBtn":
+                    daysInputElement.value = Number(hoursInputElement.value) / 24;
+                    minutesInputElement.value = Number(hoursInputElement.value) * 60;
+                    secondsInputElement.value = Number(minutesInputElement.value) * 60;
+                    break;
+                case "minutesBtn":
+                    hoursInputElement.value = Number(minutesInputElement.value) / 60;
+                    daysInputElement.value = Number(hoursInputElement.value) / 24;
+                    secondsInputElement.value = Number(minutesInputElement.value) * 60;
+                    break;
+                case "secondsBtn":
+                    minutesInputElement.value = Number(secondsInputElement.value) / 60;
+                    hoursInputElement.value = Number(minutesInputElement.value) / 60;
+                    daysInputElement.value = Number(hoursInputElement.value) / 24;
+                    break;
+            }
+        }
+    }
 }
--- js-advanced-january/week3/12-excercise-dom-manipulation/05.JS-Advanced-DOM-Events-Exercise-Resources/03.Locked-Profile/app.js
+++ js-advanced-january/week3/12-excercise-dom-manipulation/05.JS-Advanced-DOM-Events-Exercise-Resources/03.Locked-Profile/app.js
@@ -1,3 +1,21 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let btnElements = document.querySelectorAll('.profile button'); //NodeList
+    for (let btn of btnElements) {
+        btn.addEventListener('click', displayHidden);
+
+        function displayHidden(e) {
+            let parentElement = btn.parentElement;
+            let divToDisplay = parentElement.querySelector('.profile div');
+            let inputElement = parentElement.querySelector('.profile input[value="unlock"]:checked');
+            
+            if (inputElement && btn.textContent == 'Show more') {
+                divToDisplay.style.display = 'block';
+                btn.textContent = 'Hide it';
+                
+            } else if(inputElement && btn.textContent == 'Hide it'){
+                divToDisplay.style.display = 'none';
+                btn.textContent = 'Show more';
+            }
+        }
+    }
 }
