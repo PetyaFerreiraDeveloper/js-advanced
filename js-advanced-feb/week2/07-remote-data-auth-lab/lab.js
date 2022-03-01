cdb08b8b52775f02eb0d718c4652a8124c37d9a4
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2022-03-01 21:20:56 +0100

    resources

---
+++ js-advanced-feb/week2/07-remote-data-auth-lab/lab.js
@@ -0,0 +1,2 @@
+// difference between web application and web page
+//1. with web app the info is personalized, f.ex the user can log in and has his own data available
--- js-advanced-january/week3/10-excercise-dom/07.Hells-Kitchen/app1.js
+++ js-advanced-january/week3/10-excercise-dom/07.Hells-Kitchen/app1.js
@@ -3,6 +3,49 @@ function solve() {
 
     function onClick() {
         let input = JSON.parse(document.querySelector('#inputs textarea').value);
-        console.log(input);
+        // console.log(input);
+
+        let avgSalary = 0;
+        let totalSalary = 0;
+        let currAvgSalary = 0;
+        let bestRestName = '';
+        let output = {};
+
+        for (let line of input) {
+            let restaurantInfo = line.split(' - ');
+            let restaurantName = restaurantInfo[0];
+            let workersData = restaurantInfo[1].split(', ');
+
+            for(let worker of workersData) {
+                let [name, salary] = worker.split(' ');
+                if (!output.hasOwnProperty(restaurantName)){
+                    output[restaurantName] = {};
+                }
+                if(output.hasOwnProperty(restaurantName)) {
+                    output[restaurantName][name] = Number(salary);
+                }
+            }
+        }
+        let entries = Object.entries(output);
+        for(let entry of entries){
+            
+            let key = entry[0];
+            let values = Object.values(entry[1])
+            for(let salary of values){
+                totalSalary += salary;
+            }
+            // console.log(totalSalary);
+            avgSalary = totalSalary / values.length;
+            if(avgSalary > currAvgSalary){
+                currAvgSalary = avgSalary;
+                bestRestName = key;
+                totalSalary = 0;
+            }
+        }
+        let result = Object.entries(output[bestRestName]).sort((a, b)=> b[1] - a[1]);
+        let print = '';
+        result.forEach(w => print += `Name: ${w[0]} With Salary: ${w[1]} `);
+        document.querySelector('#bestRestaurant p').textContent = `Name: ${bestRestName} Average Salary: ${avgSalary.toFixed(2)} Best Salary: ${result[0][1].toFixed(2)}`;
+        document.querySelector('#workers p').textContent = print;
     }
 }
